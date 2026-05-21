209439ae66ae3a592d8a05550f0d659f3e06189e
CoupDerace <turtusibae@gmail.com>
2026-05-21 14:02:51 +0700

    feat order_model
     convert JSON ke objek OrderItemModel

--- lib/features/order/data/models/order_model.dart
+++ lib/features/order/data/models/order_model.dart
@@ -12,4 +12,29 @@ final int productId;
     required this.quantity,
     required this.subtotal,
   });
+
+factory OrderItemModel.fromJson(Map<String, dynamic> json) {
+
+    final price =
+        (json['price'] as num?)?.toDouble() ?? 0.0;
+
+    final quantity =
+        json['quantity'] as int? ?? 0;
+
+    final apiSubtotal =
+        (json['subtotal'] as num?)?.toDouble() ?? 0.0;
+
+    final subtotal =
+        apiSubtotal > 0
+            ? apiSubtotal
+            : price * quantity;
+
+    return OrderItemModel(
+      productId: json['product_id'] as int? ?? 0,
+      productName: json['product_name'] as String? ?? '',
+      price: price,
+      quantity: quantity,
+      subtotal: subtotal,
+    );
+  }
 }
