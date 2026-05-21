e0aa798a1300b7670f860da75067b99ae68c69fe
CoupDerace <turtusibae@gmail.com>
2026-05-21 22:42:14 +0700

    fix: feat order_provider
     add class n variable state

--- lib/features/order/presentation/providers/order_provider.dart
+++ lib/features/order/presentation/providers/order_provider.dart
@@ -3,23 +3,18 @@ import 'package:uts_catalog_helm/features/order/data/models/order_model.dart';
 import 'package:uts_catalog_helm/features/order/data/repositories/order_repository_impl.dart';
 import 'package:uts_catalog_helm/features/order/domain/repositories/order_repository.dart';
 
-enum OrderStatus {
-  initial,
-  loading,
-  success,
-  error,
-}
+enum OrderStatus { initial, loading, success, error }
 
 class OrderProvider extends ChangeNotifier {
-  final OrderRepository _repository =
-      OrderRepositoryImpl();
+  final OrderRepository _repository = OrderRepositoryImpl();
 
-  OrderStatus _checkoutStatus =
-      OrderStatus.initial;
-
+  OrderStatus _checkoutStatus = OrderStatus.initial;
   OrderModel? _lastOrder;
-
   List<OrderModel> _orders = [];
-
   String? _error;
+
+  OrderStatus get checkoutStatus => _checkoutStatus;
+  OrderModel? get lastOrder => _lastOrder;
+  List<OrderModel> get orders => _orders;
+  String? get error => _error;
 }
