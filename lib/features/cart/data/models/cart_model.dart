fc600a847c702ab965ef13a6e13fc1419330bdc4
CoupDerace <turtusibae@gmail.com>
2026-05-21 13:12:05 +0700

    feat cart_model
     convert JSON ke objek dart

--- lib/features/cart/data/models/cart_model.dart
+++ lib/features/cart/data/models/cart_model.dart
@@ -1,23 +1,25 @@
 class CartProductModel {
-  final String id;
+  final int id;
   final String name;
   final double price;
-  final int quantity;
+  final String imageUrl;
+  final String category;
 
   CartProductModel({
     required this.id,
     required this.name,
     required this.price,
-    required this.quantity,
+    required this.imageUrl,
+    required this.category,
   });
 
   factory CartProductModel.fromJson(Map<String, dynamic> json) {
     return CartProductModel(
-      id: json['id'],
-      name: json['name'],
-      price: json['price'].toDouble(),
-      quantity: json['quantity'],
+      id: json['ID'] as int? ?? json['id'] as int? ?? 0,
+      name: json['name'] as String? ?? '',
+      price: (json['price'] as num?)?.toDouble() ?? 0.0,
+      imageUrl: json['image_url'] as String? ?? '',
+      category: json['category'] as String? ?? '',
     );
   }
-
 }
