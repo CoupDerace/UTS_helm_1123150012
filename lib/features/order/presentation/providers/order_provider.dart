bbbfd32317e86ef12b6db1d8f6266afadb3913e0
CoupDerace <turtusibae@gmail.com>
2026-05-21 22:46:13 +0700

    feat order_provider
     add checkout function

--- lib/features/order/presentation/providers/order_provider.dart
+++ lib/features/order/presentation/providers/order_provider.dart
@@ -17,4 +17,32 @@ class OrderProvider extends ChangeNotifier {
   OrderModel? get lastOrder => _lastOrder;
   List<OrderModel> get orders => _orders;
   String? get error => _error;
+
+  Future<bool> checkout({
+    required String shippingAddress,
+    String? notes,
+    required String paymentMethod,
+  }) async {
+    _setLoading();
+
+    try {
+      final result = await _repository.checkout(
+        shippingAddress: shippingAddress,
+        notes: notes,
+        paymentMethod: paymentMethod,
+      );
+
+      _lastOrder = result;
+
+      _checkoutStatus = OrderStatus.success;
+
+      notifyListeners();
+
+      return true;
+    } catch (e) {
+      _setError(e.toString());
+
+      return false;
+    }
+  }
 }
