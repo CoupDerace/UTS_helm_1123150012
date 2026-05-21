54e1594ca673987f487dc36bba6851b582e72ccd
CoupDerace <turtusibae@gmail.com>
2026-05-21 13:09:58 +0700

    feat cart_model
     update class n constructor

--- lib/features/cart/data/models/cart_model.dart
+++ lib/features/cart/data/models/cart_model.dart
@@ -11,4 +11,13 @@ class CartProductModel {
     required this.quantity,
   });
 
+  factory CartProductModel.fromJson(Map<String, dynamic> json) {
+    return CartProductModel(
+      id: json['id'],
+      name: json['name'],
+      price: json['price'].toDouble(),
+      quantity: json['quantity'],
+    );
+  }
+
 }
